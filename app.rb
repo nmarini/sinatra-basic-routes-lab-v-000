94d18ed57b193b3cdefa1d03f385459ad29398b6
Nicolas Marini <nmarini44@gmail.com>
2018-11-12 23:38:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Nick"
+  end
+
+  get '/hometown' do
+    "My hometown is Waltham"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Heartbeats by The Knife"
+  end
 end
